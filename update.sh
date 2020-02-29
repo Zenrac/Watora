c96d3739ec95f6aaaef102d4b798681d6703999e
Zenrac <zenrac@outlook.fr>
2020-02-29 01:30:05 +0100

    Few changes

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3.6 -V > /dev/null 2>&1 || {
-    echo >&2 "Python 3.6 doesn't seem to be installed.  Do you have a weird installation?"
-    echo >&2 "If you have python 3.6, use it to run run.py instead of this script."
+python3.7 -V > /dev/null 2>&1 || {
+    echo >&2 "Python 3.7 doesn't seem to be installed.  Do you have a weird installation?"
+    echo >&2 "If you have another python version up to 3.6+, use it to run run.py instead of this script."
     exit 1; }
 
-python3.6 -m pip install --upgrade -r requirements.txt
+python3.7 -m pip install --upgrade -r requirements.txt
